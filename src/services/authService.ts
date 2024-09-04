c09a1e3a1a32ecccfd6cdebab1c83724f3420588
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-09-05 00:08:50 +0300

    update: prevent user registration if email already exists

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,8 +2,18 @@ import bcryptjs from "bcryptjs"
 import UserModel from "../models/user.model"
 import { RegisterDTO } from "../dtos/auth.dto"
 
+export const userAlreadyExists = async (email: string): Promise<boolean> => {
+  const user = await UserModel.findOne({ email })
+  return !!user
+}
+
 export const register = async (data: RegisterDTO): Promise<RegisterDTO> => {
   const hashedPassword = await bcryptjs.hash(data.password, 10)
+  const exists = await userAlreadyExists(data.email)
+
+  if (exists) {
+    return Promise.reject(new Error("User already exists"))
+  }
 
   const newUser = new UserModel({
     email: data.email,
