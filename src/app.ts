5d88910259024d9c25c48c032588ff7022f69a0e
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-09-06 00:52:31 +0300

    feat: cookie-parser for handling cookies

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/authRoutes"
+import userRoutes from "./routes/userRoutes"
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 export default app
